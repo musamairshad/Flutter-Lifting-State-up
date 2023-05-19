30ef42c5c12a0352a71eb266f6a87bdb939d70bc
Muhammad Usama <imusamairshad@gmail.com>
2023-05-19 23:06:15 +0500

    main.dart file is modified.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,34 @@ import 'package:example/blue_square.dart';
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Scaffold(body: Center(child: BlueSquare(size: 50.0))),
+    home: MyHomePage(),
   ));
+}
+
+
+class MyHomePage extends StatefulWidget{
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState(){
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  int clicks = 0;
+  @override
+  Widget build(context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BlueSquare'),
+      ),
+      body: Column(
+        children: [
+          BlueSquare(size: 100, clicks: clicks),
+          BlueSquare(size: 50, clicks: clicks),
+          BlueSquare(size: 70, clicks: clicks),
+        ],
+      ),
+    );
+  }
 }
